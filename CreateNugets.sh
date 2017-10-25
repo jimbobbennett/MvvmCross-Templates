550e595c5b510f949b0b04a70ffd1f1aa8d047ee
Jim Bennett <jimbobbennett@mac.com>
2017-10-25 19:50:22 +1300

    Added solutions to nugets to make it work on nuget.org

--- CreateNugets.sh
+++ CreateNugets.sh
@@ -1,9 +1,13 @@
+rm *.nupkg
+
 mono ./nuget.exe pack ./MvvmCrossNativeSinglePage.iOS.Android.nuspec
+
+mv ./MvvmCross.Templates.CSharp.Native.SinglePage.iOS.Android.*.nupkg MvvmCross.Templates.CSharp.Native.SinglePage.iOS.Android.nupkg
+
 mono ./nuget.exe pack ./MvvmCrossNativeSinglePage.iOS.Android.Windows.nuspec
+
+mv ./MvvmCross.Templates.CSharp.Native.SinglePage.iOS.Android.Windows.*.nupkg MvvmCross.Templates.CSharp.Native.SinglePage.iOS.Android.Windows.nupkg
+
 mono ./nuget.exe pack ./MvvmCrossFormsMultiplePages.iOS.Android.nuspec
 
-mv ./MvvmCross.Templates.CSharp.Native.SinglePage.iOS.Android.1.0.0.nupkg MvvmCross.Templates.CSharp.Native.SinglePage.iOS.Android.nupkg
-
-mv ./MvvmCross.Templates.CSharp.Native.SinglePage.iOS.Android.Windows.1.0.0.nupkg MvvmCross.Templates.CSharp.Native.SinglePage.iOS.Android.Windows.nupkg
-
-mv ./MvvmCross.Templates.CSharp.Forms.MultiplePages.iOS.Android.1.0.0.nupkg MvvmCross.Templates.CSharp.Forms.MultiplePages.iOS.Android.nupkg
+mv ./MvvmCross.Templates.CSharp.Forms.MultiplePages.iOS.Android.*.nupkg MvvmCross.Templates.CSharp.Forms.MultiplePages.iOS.Android.nupkg
